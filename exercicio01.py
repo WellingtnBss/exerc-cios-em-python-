64c17c9350d256ac7f5250fd82e30692a23847ba
WellingtnBss <154921542+WellingtnBss@users.noreply.github.com>
2023-12-29 13:41:54 -0300

    Update exercicio01.py

--- exercicio01.py
+++ exercicio01.py
@@ -1,3 +1,31 @@
+#Esboço em portugol
+
+algoritmo "triangulos"
+var 
+
+   l1, l2, l3: Real
+   EQ, ES, TRI: logico
+
+inicio
+
+       Escreva("Digite o primeiro lado: ")
+       Leia(l1)
+       Escreva("Digite o segundo lado")
+       Leia(l2)
+       Escreva("Digite o teerceiro lado: ")
+       leia(l3)
+       TRI <- (l1 < l2 + l3) e (l2 < l1 + l3) e (l3 < l1 + l2)
+       EQ <- (L1 = L2) e (l2 = l3) 
+       ES <- (L1 <> L2) e (l2 <> l3) e (l1 <> l3) 
+       Escreval ("pode formar um TRIANGULO?  ", TRI)
+       Escreval ("o triângulo é equilatero?   ", EQ)
+       Escreval ("o triângulo é escaleno?     ", ES)
+
+filalgoritmo
+
+
+
+
 
 #O código verifica se os números inseridos pelo usuário podem formar triângulos.
 
